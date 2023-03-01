a58f07ff4bbd226e70ef867055008df592bca0d0
Anton Gubarev <apgubarev@avito.ru>
2023-03-01 16:59:10 +0300

    improve todo plugin

--- nvim/lua/plugins/todo.lua
+++ nvim/lua/plugins/todo.lua
@@ -1,10 +1,37 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
-local todo = require('todo-comments')
+local todo = require("todo-comments")
 
 todo.setup({
 	highlight = {
 		keyword = "bg",
+	},
+	keywords = {
+		fix = {
+			icon = " ",
+			color = "error",
+			alt = { "FIXME", "fixme", "BUG", "bug", "FIXIT", "fixit", "ISSUE", "issue" },
+		},
+		todo = { icon = " ", color = "info" },
+		hack = { icon = " ", color = "warning" },
+		warn = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+		perf = { icon = " ", alt = { "OPTIM", "optim", "PERFORMANCE", "performance", "OPTIMIZE", "optimize" } },
+		note = { icon = " ", color = "hint", alt = { "INFO", "info" } },
+		test = { icon = "⏲ ", color = "test", alt = { "TESTING", "testing", "PASSED", "passed", "FAILED", "failed" } },
+	},
+	search = {
+		command = "rg",
+		args = {
+			"--no-heading",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"--hidden",
+			"--trim",
+			"--glob=!vendor",
+			"--glob=!.git",
+			"--glob=!node_modules",
+		},
 	},
 })
 
