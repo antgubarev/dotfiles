eae7ed46664da02b62a615525e5bc4852178fb76
Anton Gubarev <apgubarev@avito.ru>
2023-03-01 16:56:46 +0300

    organize abbreviations

--- .zsh/abbr.sh
+++ .zsh/abbr.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # https://github.com/olets/zsh-abbr
 
-abbr clear-session
-
-abbr ll="exa -hla --git --icons"
+#git
 abbr gc='git commit -m' 
 abbr gca='git commit --amend -m' 
 abbr gs='git status -sb' 
@@ -13,20 +11,32 @@ abbr gl='git pull origin'
 abbr gp='git push origin' 
 abbr gst='git stash' 
 abbr grhh='git reset --hard HEAD' 
-abbr a='avito' 
+
+#docker
 abbr d='docker' 
 abbr di='docker images'
 abbr dps='docker ps -a'
 abbr cdt='cd (mktemp -d)' 
+
+# kubectl
 abbr k='kubectl' 
 abbr ka='kubectl apply -f'
 abbr kg='kubectl get'
 abbr kgp="kubectl get pod"
-abbr aka='avito kubectl auth'
 abbr kcuc='kubectl config use-context' 
-abbr v='nvim' 
+
+#ansible
 abbr anp='ansible-playbook' 
 abbr an='ansible' 
 abbr ang='ansible-galaxy' 
+
+#tmux
+abbr tmls='tmux ls'
+abbr tmas='tmux attach-session -t'
+abbr tmks='tmux kill-session -t'
+abbr tmns='tmux new-session'
+ 
+#misc
 abbr ff='lfcd'
-abbr kb='nvim ~/Documents/kb/antgubarev'
+abbr v='nvim' 
+abbr ll="exa -hla --git --icons"
